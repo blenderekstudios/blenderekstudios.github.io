7c7a1df5a177a6974448b8f4a6ef7d7cc493cfe4
Artur Bieniek <ar2rb0k@interia.pl>
2021-01-14 15:07:26 +0100

    Copy QNapi download site

---
+++ index_pliki/translation.js
@@ -0,0 +1,182 @@
+var dict = {
+"nowości": {
+  en: "news"
+},
+"poznaj": {
+  en: "discover"
+},
+"pobierz": {
+  en: "download"
+},
+"pomoc": {
+  en: "help"
+},
+"wesprzyj": {
+  en: "donate"
+},
+
+"Program do pobierania i automatycznego dopasowywania napisów do filmów. Działa pod Linuksem, OSX i Windowsem.": {
+  en: "A software for downloading and automatic matching subtitles to movies. Works on Linux, OSX and Windows."
+},
+"Pobierz teraz": {
+  en: "Download now"
+},
+"lub": {
+  en: "or"
+},
+"dowiedz się więcej...": {
+  en: "read more..."
+},
+
+"Nowości": {
+  en: "News"
+},
+"QNapi 0.2.3 wydane. Program został przetłumaczony na jęz. angielski i włoski." : {
+  en: "QNapi 0.2.2 released. It was translated to English and Italian languages."
+},
+"QNapi w wersji 0.2.2 wydane.": {
+  en: "QNapi 0.2.2 was released."
+},
+"Pełna lista zmian": {
+  en: "Full change log"
+},
+"QNapi w wersji 0.2.1 wydane.": {
+  en: "QNapi 0.2.1 was released."
+},
+"QNapi 0.2.0 wydane! W tej wersji dużo poprawek do błędów znalezionych przez użytkowników, wsparcie dla ekranów retina na OSX i możliwośc włączenia cichego trybu przy pobieraniu napisów z menu kontekstowego.": {
+  en: "QNapi 0.2.0 released! In this version there are lot of bug fixes found by users, added support for retina sceens for OSX and ability to enable silent mode when downloading from context menu."
+},
+
+"Chcę otrzymywać powiadomienia o nowych wersjach": {
+  en: "I want to subscribe to the new releases newsletter"
+},
+"Zapisz": {
+  en: "Subscribe"
+},
+
+"Poznaj QNapi": {
+  en: "Discover QNapi"
+},
+"Program QNapi służy do automatycznego pobierania i dopasowywania napisów do konkretnego pliku z filmem. W odróżnieniu od podobnych programów, QNapi odpytuje kilka różnych baz z napisami, aby znaleźć najlepsze napisy dla Twojego filmu. Ponadto, dzięki bibliotece QT, program działa na różnych systemach operacyjnych (Windows, OSX, Linux).": {
+  en: "QNapi is designed to automatically download and match subtitles to a specific movie file. Unlike similar programs, QNapi queries several different subtitle databases to find the best subtitles for your movie. Also, thanks to the QT library, the program runs on various operating systems (Windows, OSX, Linux)."
+},
+"Program korzysta z bazy napisów": {
+  en: "The program uses subtitles database"
+},
+
+"Możliwości programu": {
+  en: "Features"
+},
+"dopasowywanie napisów do konkretnego pliku filmowego": {
+  en: "matching subtitles for specific movie file"
+},
+"automatyczne dopasowanie najlepszych napisów": {
+  en: "automatic matching the best subtitles"
+},
+"wybór formatu napisów (SRT, mDVD, TMP, MPL2), możliwość ręcznej konwersji": {
+  en: "select target subtitles format (SRT, mDVD, TMP, MPL2) as well as manual conversion"
+},
+"możliwość wyboru napisów, gdy znaleziono kilka pasujących": {
+  en: "select subtitles, when multiple matching has been found"
+},
+"wybór języka napisów spośród kilkudziesięciu obsługiwanych": {
+  en: "download subtitles in one of many supported languages"
+},
+"skanowanie katalogów i masowe dopasowywanie napisów do wielu filmów": {
+  en: "directory scanning and batch subtitles download"
+},
+"przetwarzanie napisów: zmiana kodowania znaków, ustawianie uprawnień": {
+  en: "subtitles post-processing: change file encoding, set file permissions"
+},
+"działanie z terminala (na systemach Linux/Unix)": {
+  en: "provides nice command-line interface"
+},
+
+"Cechy programu": {
+  en: "Traits"
+},
+"łatwa, intuicyjna obsługa": {
+  en: "easy, intuitive usage"
+},
+"duża skuteczność w wyszukiwaniu napisów": {
+  en: "high efficiency in finding subtitles"
+},
+"projekt open-source": {
+  en: "open-source project"
+},
+
+"Z QNapi oszczędzasz czas potrzebny na znalezienie napisów do filmu.": {
+  en: "Using QNapi, you can save your time to find subtitles for your movie."
+},
+
+"Pobierz program": {
+  en: "Download"
+},
+"wybierz wersję dla swojego systemu operacyjnego": {
+  en: "select version for your operating system"
+},
+"instalator (.exe)": {
+  en: "installer (.exe)"
+},
+"Pobierz": {
+  en: "Download"
+},
+"archiwum przenośne (.zip)": {
+  en: "portable archive (.zip)"
+},
+"archiwum (.dmg)": {
+  en: "image (.dmg)"
+},
+"Lub jeśli korzystasz z": {
+  en: "Or if you use"
+},
+"archiwum źródłowe (.tar.gz)": {
+  en: "source tarball (.tar.gz)"
+},
+"plik binarny AppImage (x86_64)": {
+  en: "binary AppImage file (x86_64)"
+},
+"dla": {
+  en: "for"
+},
+"Pakiety <a href=\"https://launchpad.net/~krzemin/+archive/ubuntu/qnapi\">PPA</a> dla": {
+  en: "<a href=\"https://launchpad.net/~krzemin/+archive/ubuntu/qnapi\">PPA</a> packages for"
+},
+"Zobacz także": {
+  en: "See also"
+},
+"pozostałe wydania": {
+  en: "other releases"
+},
+
+"Pomoc": {
+  en: "Help"
+},
+"Jeśli potrzebujesz pomocy...": {
+  en: "If you need help..."
+},
+"sprawdź czy ktoś miał (lub nadal ma) podobny problem, przeglądając <a href=\"https://github.com/QNapi/qnapi/issues?q=is%3Aissue\">zgłoszenia</a>": {
+  en: "check if someone else had similar problem, by browsing <a href=\"https://github.com/QNapi/qnapi/issues?q=is%3Aissue\">issues</a>"
+},
+"jeśli nie rozwiążesz problemu, <a href=\"https://github.com/QNapi/qnapi/issues/new\">dodaj</a>nowe zgłoszenie, dokładnie opisując swój problem i podając jak najwięcej informacji": {
+  en: "if you can't solve the problem, you can <a href=\"https://github.com/QNapi/qnapi/issues/new\">add new issue</a>, precisely describing your problem and providing as much information, as you can"
+},
+"Jeśli znajdziesz błąd w programie...": {
+  en: "If you find a bug..."
+},
+"<a href=\"https://github.com/QNapi/qnapi/issues?q=is%3Aissue+label%3Abug\">sprawdź</a> czy ktoś inny nie zgłosił podobnego błędu i czy nie został już naprawiony": {
+  en: "<a href=\"https://github.com/QNapi/qnapi/issues?q=is%3Aissue+label%3Abug\">check</a> if the bug wasn't reported by someone else or if it's already fixed"
+},
+"jeśli nie, <a href=\"https://github.com/QNapi/qnapi/issues/new\">dodaj</a> nowe zgłoszenie dokładnie opisując okoliczności wystąpienia błędu, podając szczegółowe informacje o wersji programu, systemie, itd.": {
+  en: "if not, <a href=\"https://github.com/QNapi/qnapi/issues/new\">add new</a> bug report precisely describing circumstances under the issue happens, providing detailed information about programs version, operating system, etc."
+},
+
+"Wsparcie": {
+  en: "Donate"
+},
+"Jeśli chcesz wspomóc dalszy rozwój programu, możesz to zrobić kilkając w poniższy przycisk.": {
+  en: "If you want to support further project development, you can do this by clicking the button below."
+},
+
+
+};
